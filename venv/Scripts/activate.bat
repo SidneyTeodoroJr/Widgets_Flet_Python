ba0d1fff52aeb33253c9014f7d886ec980da849f
SidneyTeodoroJr <sidneyteodoro2002@gmail.com>
2025-05-27 15:35:11 -0300

    0.0.1

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=K:\workspace_dev\projects_python\flet\Widgets_Flet_Python\venv"
+set "VIRTUAL_ENV=E:\workspace_dev\projects_python\flet\Widgets_Flet_Python\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
